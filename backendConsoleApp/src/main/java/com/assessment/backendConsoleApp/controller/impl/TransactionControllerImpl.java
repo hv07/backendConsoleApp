4701d60df490c73faa05ce82509073f833e4744f
hv07 <dios.revolver@gmail.com>
2018-05-14 12:24:15 -0500

    Update TransactionControllerImpl.java
    FIX exception

--- backendConsoleApp/src/main/java/com/assessment/backendConsoleApp/controller/impl/TransactionControllerImpl.java
+++ backendConsoleApp/src/main/java/com/assessment/backendConsoleApp/controller/impl/TransactionControllerImpl.java
@@ -14,38 +14,31 @@ public class TransactionControllerImpl implements ITransactionController {
 
 	@Override
 	public void driveTransactionAction(String request) {
+		try {
+			if (request.contains("add")) {
+				if (request.contains("{") || request.contains("}")) {
 
-		if (request.contains("add")) {
-			if (request.contains("{") || request.contains("}")) {
-				try {
 					transactionService.addTransaction(request);
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
-				}
-			} else {
-				System.out.println("There is not Json request");
-			}
-		} else if (request.contains("show")) {
-			try {
-				transactionService.showTransaction(request);
-			} catch (ClassNotFoundException e) {
-				
-				e.printStackTrace();
-			}
-		} else if (request.contains("list")) {
-			try {
-				transactionService.listTransactions(request);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		} else if (request.contains("sum")) {
-			try {
-				transactionService.sumTransactions(request);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
 
+				} else {
+					System.out.println("There is not Json request");
+				}
+			} else if (request.contains("show")) {
+
+				transactionService.showTransaction(request);
+
+			} else if (request.contains("list")) {
+
+				transactionService.listTransactions(request);
+
+			} else if (request.contains("sum")) {
+
+				transactionService.sumTransactions(request);
+
+			}
+		} catch (Exception e) {
+			System.err.print(e)
+		}
 	}
 
 }
